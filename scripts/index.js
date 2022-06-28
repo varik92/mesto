87beac7f2efe9d4f01b845e5099038be3d8f0849
Vartan Davtyan <vrtndvtn@yandex.ru>
2022-06-28 20:10:15 +0300

    Вторая сдача

--- scripts/index.js
+++ scripts/index.js
@@ -27,22 +27,38 @@ const initialCards = [
 
 const elementTemplate = document.querySelector('.element-template').content
 const elementList = document.querySelector('.elements__list')
-const zoomPopup = document.querySelector('.popup_type_zoom-image')
-
+const popupZoom = document.querySelector('.popup_type_zoom-image')
+const formElement = document.querySelector('.popup__form_type_profile')
+const nameInput = formElement.querySelector('.input_type_name')
+const jobInput = formElement.querySelector('.input_type_about')
+const popupProfile = document.querySelector('.popup_type_edit-profile')
+const buttonEdit = document.querySelector('.profile__edit-button')
+const buttonClosePlace = document.querySelector('.popup__button-close_type_place')
+const buttonCloseProfile = document.querySelector('.popup__button-close_type_profile')
+const popupAddPlace = document.querySelector('.popup_type_add-place')
+const buttonAdd = document.querySelector('.profile__add-button')
+const currentName = document.querySelector('.profile__name')
+const currentAbout = document.querySelector('.profile__description')
+const formAddElement = document.querySelector('.popup__form_type_place')
+const placeInput = formAddElement.querySelector('.input_type_place')
+const linkInput = formAddElement.querySelector('.input_type_link')
+const popupImage = document.querySelector('.popup__zoom-image')
+const popupFigcaption = document.querySelector('.popup__image-caption')
+const buttonCloseZoom = document.querySelector('.popup__button-close_type_figure')
 
 function createElement(name, link) {
     const htmlElement = elementTemplate.cloneNode(true);
     htmlElement.querySelector('.element__title').textContent = name;
-    htmlElement.querySelector('.element__image').src = link;
-    htmlElement.querySelector('.element__image').alt = name;
+    const elementImage = htmlElement.querySelector('.element__image');
+    elementImage.src = link;
+    elementImage.alt = name;
     htmlElement.querySelector('.element__like-button').addEventListener('click', function (evt) {
         evt.target.classList.toggle('element__like-button_active');
     });
     htmlElement.querySelector('.element__delete-button').addEventListener('click', function (evt) {
         evt.target.closest('.element').remove();
     });
-    htmlElement.querySelector('.element__image').addEventListener('click', function (evt) {
-        popupOpened(zoomPopup);
+    elementImage.addEventListener('click', function (evt) {
         zoomImage(name, link);
     })
     return htmlElement;
@@ -58,79 +74,52 @@ initialCards.forEach(function (el) {
     renderElement(el.name, el.link)
 });
 
-const popupProfile = document.querySelector('.popup_type_edit-profile')
-const editButton = document.querySelector('.profile__edit-button')
-const closeButtonPlace = document.querySelector('.popup__button-close_type_place')
-const closeButtonProfile = document.querySelector('.popup__button-close_type_profile')
-const popupAddPlace = document.querySelector('.popup_type_add-place')
-const addButton = document.querySelector('.profile__add-button')
+buttonEdit.addEventListener('click', () => openPopup(popupProfile))
+buttonEdit.addEventListener('click', () => openPopupProfile())
+buttonAdd.addEventListener('click', () => openPopup(popupAddPlace))
 
-editButton.addEventListener('click', () => popupOpened(popupProfile))
-addButton.addEventListener('click', () => popupOpened(popupAddPlace))
-
-// Выберите элементы, куда должны быть вставлены значения полей
-let currentName = document.querySelector('.profile__name')
-let currentAbout = document.querySelector('.profile__description')
-
-function popupOpened(popup) {
+function openPopup(popup) {
     popup.classList.add('popup_opened')
+}
+
+function openPopupProfile() {
     nameInput.value = currentName.textContent
     jobInput.value = currentAbout.textContent
 }
 
-function popupClosed(popup) { popup.classList.remove('popup_opened') }
+function closePopup(popup) { popup.classList.remove('popup_opened') }
 
-closeButtonProfile.addEventListener('click', () => popupClosed(popupProfile))
-closeButtonPlace.addEventListener('click', () => popupClosed(popupAddPlace))
+buttonCloseProfile.addEventListener('click', () => closePopup(popupProfile))
+buttonClosePlace.addEventListener('click', () => closePopup(popupAddPlace))
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form_type_profile')  // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.input_type_name') // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.input_type_about') // Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
+    evt.preventDefault();
 
-    // Получите значение полей jobInput и nameInput из свойства value
-    let newName = nameInput.value
-    let newAbout = jobInput.value
-    // Вставьте новые значения с помощью textContent
+    const newName = nameInput.value
+    const newAbout = jobInput.value
+
     currentName.textContent = newName
     currentAbout.textContent = newAbout
-    popupClosed(popupProfile)
+    closePopup(popupProfile)
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
 
-let formAddElement = document.querySelector('.popup__form_type_place')
-let placeInput = formAddElement.querySelector('.input_type_place')
-let linkInput = formAddElement.querySelector('.input_type_link')
+formElement.addEventListener('submit', formSubmitHandler);
 
 function addNewElement(evt) {
     evt.preventDefault();
 
     renderElement(placeInput.value, linkInput.value)
-
-    popupClosed(popupAddPlace)
+    placeInput.value = '';
+    linkInput.value = '';
+    closePopup(popupAddPlace)
 }
 formAddElement.addEventListener('submit', addNewElement);
 
-const popupImage = document.querySelector('.popup__zoom-image')
-const popupFigcaption = document.querySelector('.popup__image-caption')
-
 function zoomImage(name, link) {
+    openPopup(popupZoom);
     popupImage.setAttribute('src', link)
     popupImage.setAttribute('alt', name)
     popupFigcaption.textContent = name
 }
 
-const closeButtonZoom = document.querySelector('.popup__button-close_type_figure')
-const popupZoom = document.querySelector('.popup_type_zoom-image')
-
-closeButtonZoom.addEventListener('click', () => popupClosed(popupZoom))
+buttonCloseZoom.addEventListener('click', () => closePopup(popupZoom))
